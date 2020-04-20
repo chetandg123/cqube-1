2b2966bfdbf09d2f8a7d912fd9e64611c986888f
tibilproject <63738269+tibilproject@users.noreply.github.com>
2020-04-20 22:01:43 +0530

    addded select box code

--- Pages/homePage.py
+++ Pages/homePage.py
@@ -12,6 +12,9 @@ class HomePage():
         self.Cluster = Locators.cluster
         self.Schools = Locators.schools
         self.HomeButton = Locators.homebutton
+        self.select_districts = Locators.select_district
+        self.select_blocks = Locators.select_blocks
+        self.select_clusters = Locators.select_clusters
 
 
 
@@ -33,6 +36,22 @@ class HomePage():
         self.driver.find_element_by_xpath(self.Cluster).click()
     def ClickOn_Schools(self):
         self.driver.find_element_by_xpath(self.Schools).click()
+        
     def ClickOn_HomeButton(self):
         self.driver.find_element_by_id(self.HomeButton).click()
+        def select_district_block_cluster(self):
+        select_district = Select(self.driver.find_element_by_xpath(self.select_districts))
+        select_block = Select(self.driver.find_element_by_xpath(self.select_blocks))
+        select_cluster = Select(self.driver.find_element_by_xpath(self.select_clusters))
+        time.sleep(5)
+        for x in range(1, len(select_district.options)):
+            select_district.select_by_index(x)
+            time.sleep(2)
+            for y in range(1, len(select_block.options)):
+                select_block.select_by_index(y)
+                time.sleep(2)
+                for z in range(1, len(select_cluster.options)):
+                    select_cluster.select_by_index(z)
+                    time.sleep(2)
+                    self.MouseOverOnPoints()
 
